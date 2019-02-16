45b55bddb08ba608e09aecd7eeefbe6855a4606a
Leo Dasso <leo.dasso@gmail.com>
2019-02-16 15:27:13 -0600

    get index route running and do some basic placeholders for checklist item

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log('server is running');
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// Have the express app listen on our port
+app.listen(PORT, () => {
+    console.log('Listening on port ' + PORT);
+    
+})
+
+app.use(express.static('server/public'));
